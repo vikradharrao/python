73624130088daaf137171fca2100200b176adbfc
unknown <vikradhar.marolix@gmail.com>
2023-07-04 18:23:14 +0530

    first one

---
+++ marolix/programs.py
@@ -0,0 +1,10 @@
+
+'''
+name=''
+password=''
+while name!='vikram' or password!='1234':
+    name=input('Enter your name: ')
+    password=input('Enter your password: ')
+print('Hello', name, 'sucessfully login the page')
+'''
+
